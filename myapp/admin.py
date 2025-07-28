2ccd3fe933c16fdcfbf23d03e7db34f853fceb67
Limbureuben <limbureuben4@gmail.com>
2025-07-28 15:30:48 +0300

    changes

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Transaction
 
-# Register your models here.
+@admin.register(Transaction)
+class TransactionAdmin(admin.ModelAdmin):
+    list_display = ['phone', 'reference', 'pesapal_transaction_id', 'status', 'created_at']
+    search_fields = ['phone', 'reference', 'pesapal_transaction_id']
